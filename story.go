98400bd2372dce532b5eb19d710a803a54f1b08f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-03 16:31:00 +0100

    Created a template to serve from the web app

--- story.go
+++ story.go
@@ -5,6 +5,29 @@ import (
 	"io"
 )
 
+var defaultHanldlerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Adventure Game</title>
+  </head>
+  <body>
+    <h1>{{.Title}}</h1>
+    {{range .Story}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+      {{range .Options}}
+      <li><a href="/{{.Arc}}">{{.Text}}</a></li>
+      {{end}}
+    </ul>
+  </body>
+</html>
+`
+
 // Story the story type
 type Story map[string]Chapter
 
