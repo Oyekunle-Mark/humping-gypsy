35083365a0ac86991ffcb2c6ead753e318da6989
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-03 16:49:11 +0100

    Started the app succesfully

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	gypsy "github.com/Oyekunle-Mark/humping-gypsy"
@@ -10,6 +12,7 @@ import (
 
 func main() {
 	filename := flag.String("file", "naration.json", "The file to read the json object from.")
+	port := flag.Int("port", 5000, "The port to start the server on")
 	flag.Parse()
 
 	f, err := os.Open(*filename)
@@ -24,5 +27,8 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := gypsy.NewHandler(story)
+
+	fmt.Printf("Starting the app on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
